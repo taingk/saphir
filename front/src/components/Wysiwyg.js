7c7f58ff2fd869d56bc38b682d080eddb86c4b99
taingk <taing_k@etna-alternance.net>
2019-05-08 16:47:10 +0200

    no eject

--- front/src/components/Wysiwyg.js
+++ front/src/components/Wysiwyg.js
@@ -1,8 +1,3 @@
 import React from 'react';
-import 'froala-editor/js/froala_editor.pkgd.min.js';
-import 'froala-editor/css/froala_style.min.css';
-import 'froala-editor/css/froala_editor.pkgd.min.css';
-import 'font-awesome/css/font-awesome.css';
-import FroalaEditor from 'react-froala-wysiwyg';
 
-export default () => <FroalaEditor tag="textarea" />;
+export default () => <p>Wysiwyg</p>;
