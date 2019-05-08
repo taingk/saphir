752bde96b92f0080c9e907308aa6be3016a715b6
taingk <taing_k@etna-alternance.net>
2019-05-08 16:57:47 +0200

    add fast wysiwyg

--- front/src/components/Wysiwyg.js
+++ front/src/components/Wysiwyg.js
@@ -1,3 +1,11 @@
 import React from 'react';
+import CKEditor from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-export default () => <p>Wysiwyg</p>;
+export default () => <CKEditor
+  editor={ClassicEditor}
+  onChange={( event, editor) => {
+    const data = editor.getData();
+    console.log({ event, editor, data });
+  }}
+/>;
