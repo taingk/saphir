8d3d5af35cb4f61121b0be15d1fd3708b0e2d464
Baal-Marduk <leo.wattier@free.fr>
2019-06-05 16:02:05 +0200

    fix user creation

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
-
+    include ActionController::Helpers
+    
     helper_method :current_user
     
          private
--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
-    ROLES = {0 => :guest, 1 => :user, 2 => :moderator, 3 => :admin}
-    attr_reader :role
+    validates_presence_of :pseudo, :password, :email
+#     ROLES = {0 => :guest, 1 => :user, 2 => :moderator, 3 => :admin}
+#     attr_reader :role
 
-  def initialize(role_id = 0)
-    @role = ROLES.has_key?(role_id.to_i) ? ROLES[role_id.to_i] : ROLES[0]
-  end
+#   def initialize(role_id = 0)
+#     @role = ROLES.has_key?(role_id.to_i) ? ROLES[role_id.to_i] : ROLES[0]
+#   end
 
-  def role?(role_name)
-    role == role_name
-  end
+#   def role?(role_name)
+#     role == role_name
+#   end
 end
