25b34598edcdd84da194985a97a0e8ae4028c42d
Baal-Marduk <leo.wattier@free.fr>
2019-06-05 15:08:33 +0200

    user model fix

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_26_154701) do
+ActiveRecord::Schema.define(version: 2019_06_05_130320) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -50,6 +50,5 @@ ActiveRecord::Schema.define(version: 2019_05_26_154701) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
 end
