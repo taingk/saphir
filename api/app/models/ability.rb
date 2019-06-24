28ce9b5730101aa234986736148323404c7323c6
Baal-Marduk <leo.wattier@free.fr>
2019-06-24 12:29:29 +0200

    roles defining

--- api/app/models/ability.rb
+++ api/app/models/ability.rb
@@ -2,15 +2,19 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    def initialize(user)
-      user ||= User.new
-      if user.admin?
-        can :manage, :all
-      else
-        can :read, :all
-      end
+    user ||= User.new
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+      can :create, :all 
+      can [:update, :destroy], Article, :user_id user.id
+      can [:update, :destroy], Post, :user_id user.id
+      can [:update, :destroy], Comment, :user_id user.id
+      can [:update, :destroy], User, :id user.id
     end
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
   end
 end
+    # See the wiki for details:
+    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+
